0bd723bd2f7dfb324939c05ee2e666f6ccfacc54
Mahalakshmi Marimuthu <mahalakshmimarimuthu6@gmail.com>
2026-04-10 10:30:25 +0530

    Delete freshdesk directory

--- freshdesk/src/main/java/com/mahalakshmi/freshdesk/repository/ContactRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mahalakshmi.freshdesk.repository;
-import com.mahalakshmi.freshdesk.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
--- freshdesk/src/main/java/com/mahalakshmi/freshdesk/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mahalakshmi.freshdesk.repository;
-
-import com.mahalakshmi.freshdesk.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
